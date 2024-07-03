0ce406bcb7662a0d566aa05c5839770ca784f319
Priyanshu <priyanshu022017@gmail.com>
2024-07-03 08:21:13 +0530

    Done with connection with MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,9 @@
-// import { Server } from "socket.io";
 import express from "express";
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import cors from "cors";
 
 const mongoURL =
-  "mongodb+srv://priyanshu022017:foCpjHfn6K8drRUc@users.zplos5j.mongodb.net/?retryWrites=true&w=majority&appName=Users";
+  "mongodb+srv://priyanshu022017:vg8SvKqLo1byu4U5@users.zplos5j.mongodb.net/?retryWrites=true&w=majority&appName=Users";
 
 mongoose
   .connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -15,39 +14,87 @@ mongoose
     console.error(e.message);
   });
 
-const users = [
-  {
-    username: "7017987049",
-    password: "password",
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  username: { type: String, unique: true },
+  password: String,
+});
+
+// Create the user model
+const Users = mongoose.model("Users", userSchema);
+
+const messageSchema = new mongoose.Schema({
+  chat: {
+    senderId: { type: mongoose.Types.ObjectId, required: true },
+    receiverId: { type: mongoose.Types.ObjectId, required: true },
+    time: { type: Date, default: Date.now }, // Use Date type for time
+    message: { type: String, required: true },
   },
-];
+  isGroupChat: { type: Boolean, default: false }, // Corrected definition
+});
+
+const Chats = mongoose.model("chats", messageSchema);
 
 const port = 5000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Server listening on PORT: ${port}`);
 });
-app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Home Page" });
+app.get("/login", (req, res) => {
+  res.json({ title: "Login Page" });
 });
 
-app.post("/login", (req, res) => {
-  console.log(req.body);
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].username == username && users[i].password == password) {
-      return res.json({ success: true, message: "Login Successful" });
-    }
-  }
+  try {
+    const details = await Users.find({ username, password });
+    console.log(details, details.length);
 
-  res.json({ success: false, message: "Invalid credentials" });
+    if (details) {
+      const sessionId = new Date().getTime().toString();
+      return res.json({
+        success: true,
+        message: "Login Successful",
+        sessionId,
+        name: details[0].name,
+      });
+    }
+
+    return res.json({ success: false, message: "Invalid credentials" });
+  } catch (e) {}
 });
 
+app.get("/register", (req, res) => {
+  res.json({ title: "Register Page" });
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, username, password } = req.body;
+
+    const details = await Users.find({ username });
+    if (details.length > 0) {
+      return res.json({
+        success: false,
+        message: "Account already exists",
+      });
+    }
+
+    const result = await Users.insertMany({ name, username, password });
+    console.log(result);
+
+    return res.json({ success: true, message: "Registered Successfully" });
+  } catch (e) {}
+});
+
+// import { Server } from "socket.io";
 // const expressServer = app.listen(port, () => {
 //   console.log(`Server listening on PORT: ${port}`);
 // });
@@ -67,7 +114,7 @@ app.post("/login", (req, res) => {
 
 //   // to send a message
 //   socket.on("message", (message) => {
-//     if (message.substring(0, 5) != "You: ") {
+//     if (message.substring(0, 5) !== "You: ") {
 //       io.emit("message", `${message}`);
 //     }
 //   });
